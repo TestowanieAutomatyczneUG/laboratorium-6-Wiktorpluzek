794037e758fafb6fd32a2943d945b29ccd5870cf
Wiktorpluzek <75083416+Wiktorpluzek@users.noreply.github.com>
2021-11-10 11:24:25 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,60 @@
+import unittest
+
+
+class hamming:
+    def distance(geneA, geneB):
+        if geneA=="" and geneB=="":
+            return 0
+        if geneA==geneB:
+            return 0
+        return 1
+
+
+class HammingTest(unittest.TestCase):
+    def test_empty_strands(self):
+        self.assertEqual(hamming.distance("", ""), 0)
+
+    def test_single_letter_identical_strands(self):
+        self.assertEqual(hamming.distance("A", "A"), 0)
+
+    def test_single_letter_different_strands(self):
+        self.assertEqual(hamming.distance("G", "T"), 1)
+
+    def test_long_identical_strands(self):
+        self.skipTest("skip")
+        self.assertEqual(hamming.distance("GGACTGAAATCTG", "GGACTGAAATCTG"), 0)
+
+    def test_long_different_strands(self):
+        self.skipTest("skip")
+        self.assertEqual(hamming.distance("GGACGGATTCTG", "AGGACGGATTCT"), 9)
+
+    def test_disallow_first_strand_longer(self):
+        self.skipTest("skip")
+        with self.assertRaisesWithMessage(ValueError):
+            hamming.distance("AATG", "AAA")
+
+    def test_disallow_second_strand_longer(self):
+        self.skipTest("skip")
+        with self.assertRaisesWithMessage(ValueError):
+            hamming.distance("ATA", "AGTG")
+
+    def test_disallow_left_empty_strand(self):
+        self.skipTest("skip")
+        with self.assertRaisesWithMessage(ValueError):
+            hamming.distance("", "G")
+
+    def test_disallow_right_empty_strand(self):
+        self.skipTest("skip")
+        with self.assertRaisesWithMessage(ValueError):
+            hamming.distance("G", "")
+
+    # Utility functions
+    def setUp(self):
+        try:
+            self.assertRaisesRegex
+        except AttributeError:
+            self.assertRaisesRegex = self.assertRaisesRegexp
+
+    def assertRaisesWithMessage(self, exception):
+        return self.assertRaisesRegex(exception, r".+")
+
